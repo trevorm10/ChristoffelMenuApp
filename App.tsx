57fccce8ae0eb3fc77f3e538e4cf7f9cbb38cf61
trevorm10 <trevormoodley10@gmail.com>
2025-08-30 19:31:47 +0200

    new import update

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './src/screens/HomeScreen';
 import AddItemScreen from './src/screens/AddItemScreen';
+import GuestFilterScreen from './src/screens/GuestFilterScreen';
 
 type MenuItem = {
   id: string;
@@ -19,19 +20,20 @@ export default function App() {
   const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
 
   const addMenuItem = (item: MenuItem) => {
-    setMenuItems((prev) => [...prev, item]);
+    setMenuItems(prev => [...prev, item]);
   };
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home">
-          {(props) => (
-            <HomeScreen {...props} menuItems={menuItems} addMenuItem={addMenuItem} />
-          )}
+          {props => <HomeScreen {...props} menuItems={menuItems} addMenuItem={addMenuItem} />}
         </Stack.Screen>
         <Stack.Screen name="AddItem">
-          {(props) => <AddItemScreen {...props} addMenuItem={addMenuItem} />}
+          {props => <AddItemScreen {...props} addMenuItem={addMenuItem} />}
+        </Stack.Screen>
+        <Stack.Screen name="GuestFilter">
+          {props => <GuestFilterScreen {...props} menuItems={menuItems} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
